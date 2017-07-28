01821392d569da1965a1d9f6e635d376e05241b6
Alex Maimescu <maimescu.alex@gmail.com>
2017-07-28 14:04:40 +0300

    Add CocoaPods integration example.

---
+++ examples/CocoaPodsIntegrationTest/CocoaPodsIntegrationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaPodsIntegrationTest
+//
+//  Created by Alexandru Maimescu on 7/28/17.
+//  Copyright © 2017 Alex Maimescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
